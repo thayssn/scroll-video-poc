a38c170ef961e018fb0b8769d607fbf7331174c5
Thays <thays.sn@hotmail.com>
2019-07-17 01:14:37 -0300

    funcionamento diferenciado no mobile

--- scripts.js
+++ scripts.js
@@ -4,19 +4,36 @@ const wrapperHeight = document.querySelector("#long").clientHeight;
 const duration = wrapperHeight - window.innerHeight;
 const controller = new ScrollMagic.Controller();
 const scene = new ScrollMagic.Scene();
-scene.duration(duration);
 scene.setPin(video, { pushFollowers: false });
+scene.duration(duration);
 scene.addTo(controller);
 
 video.addEventListener("canplaythrough", startScrollAnimation);
 
 function startScrollAnimation() {
   video.removeEventListener("canplaythrough", startScrollAnimation);
+  h1.textContent = 'comecou';
   console.log("Stating animation 1");
   scrollTo(0, 0);
-  scene.on("progress", ({ progress }) => {
-    video.currentTime = progress * video.duration;
-    h1.textContent = progress;
-    console.log(progress);
-  });
+  if(window.innerWidth < 720) {
+    scene.duration(200);
+    scene.on('progress', ({progress, state, scrollDirection}) => {
+      if(state === 'AFTER') {
+        h1.textContent = 'AFTER';
+        video.play();
+      }
+      if(scrollDirection === 'REVERSE'){
+        h1.textContent = 'REVERSE';
+  
+        video.currentTime = 0
+        video.pause();
+      }
+    })
+  }else{
+    scene.on("progress", ({ progress }) => {
+      video.currentTime = progress * video.duration;
+      h1.textContent = progress;
+      console.log(progress);
+    });
+  }
 }
